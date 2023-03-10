da071f8b3e294f26c9220e137d220e7dff741322
CHRISANTHOS <uakuchie@gmail.com>
2023-03-10 21:41:41 +0100

    copy to clipboard

--- lib/pages/recognize_page.dart
+++ lib/pages/recognize_page.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:clipboard/clipboard.dart';
 
 class RecognizationPage extends StatefulWidget {
-
   String path;
 
-  RecognizationPage({Key? key,required this.path}) : super(key: key);
+  RecognizationPage({Key? key, required this.path}) : super(key: key);
 
   @override
   State<RecognizationPage> createState() => _RecognizationPageState();
 }
 
 class _RecognizationPageState extends State<RecognizationPage> {
-
   bool isBusy = false;
   TextEditingController controller = TextEditingController();
 
-  void processImage(InputImage inputImage) async{
+  void processImage(InputImage inputImage) async {
     final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
 
     setState(() {
       isBusy = true;
     });
 
-    final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
 
     controller.text = recognizedText.text;
 
@@ -39,27 +39,42 @@ class _RecognizationPageState extends State<RecognizationPage> {
     InputImage inputImage = InputImage.fromFilePath(widget.path);
     processImage(inputImage);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Recognize Text'),
       ),
-      body: isBusy == true ? const Center(child: CircularProgressIndicator(),) : Column(
-        children: [
-          TextFormField(
-            controller: controller,
-            maxLines: null,
-            keyboardType: TextInputType.multiline,
-            decoration: const InputDecoration(
-              hintText: 'your text'
+      body: isBusy == true
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                TextFormField(
+                  controller: controller,
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(hintText: 'your text'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    FlutterClipboard.copy(controller.text).then(
+                      (value) => ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('copied'),
+                        ),
+                      ),
+                    );
+                  },
+                  style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.blueAccent)),
+                  child: const Text('Copy'),
+                )
+              ],
             ),
-          ),
-          TextButton(onPressed: (){
-
-          },style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)), child: const Text('Copy'))
-        ],
-      ),
     );
   }
 }
